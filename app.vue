22ab282a4e3c5a04c0be1a35c56e067ee6e83214
Stephen Hallgren <stephen@simplecast.com>
2023-07-18 13:30:47 -0400

    nuxt with static site

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>Amplicube</h1>
+    <p>The worlds premier, decentralized, backward compatible, open source, podcast ecosystem.</p>
+  </main>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+        { "http-equiv": "x-ua-compatible", content: "ie=edge" },
+        { name: "description", content: "The worlds premier, decentralized, backward compatible, open source, podcast ecosystem" },
+        { name: "keywords", content: "Podcast, Decentralized, Blochchain, Ethereum" },
+        { name: "author", content: "teevio.eth" },
+        { name: "og:description", content: "The worlds premier, decentralized, backward compatible, open source, podcast ecosystem"},
+        { property: "og:image", content: "https://amplicu.be/amplicube-facebook.png" },
+        { property: "og:image:height", content: "630" },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:title", content: "Amplicube" },
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: "https://amplicu.be" },
+        { property: "twitter:creator", content: "teevio" },
+        { property: "twitter:image", content: "https://amplicu.be/amplicube-twitter.png" },
+        { property: "twitter:site:id", content: "amplicube" }
+      ],
+      link: [{ rel: 'stylesheet', href: './style.css' }],
+      script: [{ src: "https://cdn.usefathom.com/script.js", "data-site": "BGQVHRZN", defer: "defer" }],
+      title: 'Amplicube'
+    }
+  },
+  devtools: { enabled: true }
+})
